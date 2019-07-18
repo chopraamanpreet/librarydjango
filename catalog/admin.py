123a60e8fdac8f6e6f45cfbf5eaa26a14b63c929
chopraamanpreet <chopra.aman111@gmail.com>
2019-07-18 19:36:11 +0530

    adding files

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
 from catalog.models import Genre,Book,BookInstance,Author
 
-admin.site.register(Book)
-admin.site.register(BookInstance)
+#admin.site.register(Book)
+#admin.site.register(BookInstance)
 admin.site.register(Genre)
-admin.site.register(Author)
-# Register your models here.
+#admin.site.register(Author)
+
+#To change how a model is displayed in the admin interface you define a ModelAdmin class
+
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    list_display=('last_name','first_name','date_of_birth','date_of_death')
+
+
+#register the admin class with model
+admin.site.register(Author,AuthorAdmin)
+
+
+#using @register decorater to register the model
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(BookInstanceAdmin)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
